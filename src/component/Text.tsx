e8296f95c273ed3a12ed122cb0c9755edb15d6fd
orewagaurav <gauravkumar47967@gmail.com>
2025-05-03 20:02:02 +0530

    update

--- src/component/Text.tsx
+++ src/component/Text.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 
 const Text:React.FC = ()=>{
-return(
-
-  
+return(<>
 
 <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-4xl font-bold text-[#8005b1]">
+      <h1 className="text-4xl font-bold text-[#14bc39]">
         Vite + React + TS + Tailwind
       </h1>
       <p className="mt-4 text-lg text-gray-800">
         Everything works! 🚀
       </p>
-
+      <h1 className="bg-amber-300">HELP</h1>
       <img src="/vite.svg" alt="image" />
     </div>
+  
 
+
+ </>
 
 
 )
